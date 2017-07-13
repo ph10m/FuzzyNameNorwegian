f139ccd2b0d7f70675857f13bc143196467cefd8
Tollef Jørgensen <toljoe@skagenfondene.no>
2017-07-13 15:50:42 +0200

    Pickle caching of names

---
+++ data/parse_names.py
@@ -0,0 +1,31 @@
+import pickle
+from collections import defaultdict
+
+
+name_files = ['firstnames.txt', 'surnames.txt']
+
+for namefile in name_files:
+    with open(namefile) as f:
+        by_name_length = defaultdict(list)
+        by_name_letter = defaultdict(list)
+        all_names = set()
+
+        for name in f.readlines():
+            name = name.strip()
+            all_names.add(name)
+
+        for name in all_names:
+            by_name_length[len(name)].append(name)
+            by_name_letter[name[0]].append(name)
+
+        for namelen, namelist in by_name_length.items():
+            by_name_length[namelen] = sorted(namelist)
+
+        for namelen, namelist in by_name_letter.items():
+            by_name_letter[namelen] = sorted(namelist)
+
+        # store the two dictionaries as pickle files
+        proper_name = namefile[:len(namefile)-4] + '.cache'
+        print(proper_name)
+        pickle.dump(by_name_length, open(proper_name, 'wb'))
+
