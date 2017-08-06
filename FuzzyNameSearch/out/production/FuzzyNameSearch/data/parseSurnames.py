722d463aca8092407104dffa3cbc4f15048c6950
Tollef Jørgensen <tollefj@gmail.com>
2017-08-06 23:46:52 +0200

    refactoring

--- FuzzyNameSearch/out/production/FuzzyNameSearch/data/parseSurnames.py
+++
@@ -1,9 +0,0 @@
-newsurs = open('newsurnames.txt','w')
-names = set()
-with open('surnames.txt','r') as f:
-    for line in f.readlines():
-        names.add(line.strip())
-for n in sorted(names):
-    if len(n)<13:
-        newsurs.write(n + '\n')
-newsurs.close()
